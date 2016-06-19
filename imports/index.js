43b0dd0317cec91177373554c52e9b76766dece7
BlueChili <cmdr.chili@gmail.com>
2016-06-19 18:03:34 -0500

    Implemented routing

--- imports/index.js
+++ imports/index.js
@@ -1,2 +1,27 @@
-import { Meteor } form 'meteor/meteor';
+import { Meteor } from 'meteor/meteor';
 import { Template } from 'meteor/templating';
+import { FlowRouter } from 'meteor/kadira:flow-router';
+import { BlazeLayout } from 'meteor/kadira:blaze-layout';
+
+// Global UI components templates
+import './index.html';
+import './index-links.html';
+import './unironicalNav.html';
+import './user-panel.html';
+
+// Entry point for apps
+import './linker/index.js';
+
+FlowRouter.route('/', {
+  name: 'Home',
+  action() {
+    BlazeLayout.render('root', {main: 'index_links'});
+  },
+});
+
+FlowRouter.route('/linker', {
+  name: 'Linker',
+  action() {
+    BlazeLayout.render('root', { main: 'linker_main'});
+  },
+});
