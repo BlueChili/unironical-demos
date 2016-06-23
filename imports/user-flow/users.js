4d343cce413b41ee7bf7402670f74508a5902d6d
BlueChili <cmdr.chili@gmail.com>
2016-06-22 20:58:58 -0500

    User onboarding template and code refactoring

--- imports/user-flow/users.js
+++ imports/user-flow/users.js
@@ -1,22 +1,36 @@
 import { Accounts } from 'meteor/accounts-base';
 import { Template } from 'meteor/templating';
 import { FlowRouter } from 'meteor/kadira:flow-router';
+import { ReactiveVar } from 'meteor/reactive-var';
 
 import './users.html';
 
-Template.user_signup.events({
-  'click .js-signup-submit'(event) {
-    console.log(event);
-  },
-});
+let isSigning = new ReactiveVar(false);
 
-Template.user_login.events({
+Template.user_panel.events({
+  'click .js-signup-submit'(event) {
+    event.preventDefault();
+    const formData = event.target.form,
+          username = formData[0].value,
+          email = formData[1].value,
+          password = formData[2].value;
+    Accounts.createUser({
+      username: username,
+      email: email,
+      password: password},
+      function(err){
+       if (err) throw err.reason;
+       else console.log('no error');
+      }
+    );
+  },
   'click .js-login-submit'(event) {
     event.preventDefault();
     const formData = event.target.form;
     const username = formData[0].value;
     const password = formData[1].value;
-    Meteor.loginWithPassword(username,
+    Meteor.loginWithPassword(
+      username,
       password,
       (err) => {
         if (err) console.log(err);
@@ -24,8 +38,18 @@ Template.user_login.events({
       }
     );
   },
-  'click .js-logout-submit'(){
-    Meteor.logout();
+  'click .js-logout-submit'(){ Meteor.logout(); },
+  'click .js-register'() {
+    isSigning.set(true); return false;
+  },
+  'click .js-login-flow'() {
+    isSigning.set(false); return false;
+  },
+});
+
+Template.user_panel.helpers({
+  signing: function(){
+    return isSigning.get();
   },
 });
 
